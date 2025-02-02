2c0d3251c1dd79135fc50dfb1408b42fbae83d15
muffindud <ccatlabuga2002@gmail.com>
2025-02-02 18:40:25 +0200

    set the data packet to take abstract field

--- lib/DataPacker/DataPacker.cpp
+++ lib/DataPacker/DataPacker.cpp
@@ -5,7 +5,6 @@ uint16_t crc16(RAW_DATA_T data_p){
     uint16_t crc = 0xFFFF;
     for(int i = 0; i < RAW_DATA_SIZE; i++){
         crc ^= data[i];
-        free(data);
 
         for(int j = 0; j < 8; j++){
             if(crc & 0x0001){
@@ -24,7 +23,6 @@ uint16_t getId(uint8_t* source_mac, uint8_t* dest_mac, RAW_DATA_T data_p, uint16
     uint16_t id = 0x0;
     uint8_t* data = (uint8_t*)&data_p;
     int mac_idx = data[RAW_DATA_SIZE / 2] % 6;
-    free(data);
 
     id |= ((source_mac[mac_idx] + dest_mac[mac_idx]) & 0xFF) << 8;
     id |= ((crc16 & 0xFF) + ((crc16 & 0xFF00) >> 8));
--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,14 @@
 
 #include <config.h>
 
+#define DEBUG
+
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 HC12 hc12(HC_12_RX_PIN, HC_12_TX_PIN, HC_12_SET_PIN);
 
 DAT_T data_packet;
+RAW_DATA_T data;
+
 MAC_ADDRESS_T manager_mac;
 
 WiFiClient espClient;
@@ -24,13 +28,21 @@ PubSubClient client(espClient);
 
 void reconnect(){
     while(!client.connected()){
+        #ifdef DEBUG
         Serial.print("Attempting MQTT connection...");
+        #endif
+
         if(client.connect("ESP32Client")){
+            #ifdef DEBUG
             Serial.println("connected");
+            #endif
         }else{
+            #ifdef DEBUG
             Serial.print("failed, rc=");
             Serial.print(client.state());
             Serial.println(" try again in 5 seconds");
+            #endif
+
             delay(5000);
         }
     }
@@ -45,29 +57,42 @@ bool uploadData(){
 }
 
 void setup(){
+    #ifdef DEBUG
     Serial.begin(9600);
+    #endif
 
     WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    #ifdef DEBUG
     Serial.print("Connecting to " + String(WIFI_SSID));
+    #endif
+
     while(WiFi.status() != WL_CONNECTED){
         delay(100);
+
+        #ifdef DEBUG
         Serial.print(".");
+        #endif
     }
+
+    #ifdef DEBUG
     Serial.println();
     Serial.println("Connected to WiFi");
     Serial.println("IP: " + WiFi.localIP().toString());
+    #endif
 
     client.setServer(MQTT_SERVER, MQTT_PORT);
+    WiFi.macAddress(manager_mac.mac);
 
     display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
     display.display();
 
-    WiFi.macAddress(manager_mac.mac);
-
+    #ifdef DEBUG
     Serial.printf("MAC: %2x:%2x:%2x:%2x:%2x:%2x\n",
         manager_mac.mac[0], manager_mac.mac[1], manager_mac.mac[2],
         manager_mac.mac[3], manager_mac.mac[4], manager_mac.mac[5]
     );
+    #endif
 
     pinMode(PAIR_BUTTON_PIN, INPUT_PULLUP);
     delay(500);
@@ -80,18 +105,27 @@ void loop(){
 
     data_packet = hc12.receiveData();
     if(data_packet.type == DAT_MODE){
-        String data = "";
-        data += "T: " + String(data_packet.data.temperature / 100. - 40.) + " C\n";
-        data += "H: " + String(data_packet.data.humidity / 100.) + " %\n";
-        data += "P: " + String(data_packet.data.pressure / 100.) + " mmHg\n";
-        data += "PPM: " + String(data_packet.data.ppm / 100.) + " ppm\n";
+        memcpy(&data, data_packet.data, sizeof(data));
+
+        String data_string = "";
+        data_string += "T: " + String(data.temperature / 100. - 40.) + " C\n";
+        data_string += "H: " + String(data.humidity / 100.) + " %\n";
+        data_string += "P: " + String(data.pressure / 100.) + " mmHg\n";
+        data_string += "PPM: " + String(data.ppm / 100.) + " ppm\n";
 
         display.clearDisplay();
         display.setTextSize(1);
         display.setTextColor(SSD1306_WHITE);
         display.setCursor(0, 0);
-        display.println(data);
+        display.println(data_string);
         display.display();
+
+        #ifdef DEBUG
+        for(int i = 0; i < sizeof(data_packet); i++){
+            Serial.printf("%2x ", ((uint8_t*)&data_packet)[i]);
+        }
+        Serial.println();
+        #endif
 
         bool result = uploadData();
 
