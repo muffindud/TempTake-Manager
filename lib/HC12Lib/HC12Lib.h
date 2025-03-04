22f446421eb39ecd1d1156e395087eec1f34e0af
muffindud <ccatlabuga2002@gmail.com>
2025-03-04 10:49:20 +0200

    add code comments to hc12 lib

--- lib/HC12Lib/HC12Lib.h
+++ lib/HC12Lib/HC12Lib.h
@@ -7,7 +7,17 @@
 class HC12{
     public:
         HC12(uint8_t rx, uint8_t tx, uint8_t setPin);
+        
+        /*
+        * @brief Set the HC-12 module to sleep mode
+        * @param `sleep`: `true` to set the module to sleep mode, `false` to wake it up
+        */
         void setSleep(bool sleep);
+
+        /*
+        * @brief Set the channel of the HC-12 module
+        * @param chammel: The channel of the module [1..127]
+        */
         void setChannel(uint8_t channel);
 
     private:
